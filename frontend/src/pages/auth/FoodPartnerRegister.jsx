8eecc9df3453607ea231007759571c7269fb3a61
Aman <143870254+aman05hub@users.noreply.github.com>
2026-02-21 00:35:04 +0530

    Update FoodPartnerRegister.jsx

--- frontend/src/pages/auth/FoodPartnerRegister.jsx
+++ frontend/src/pages/auth/FoodPartnerRegister.jsx
@@ -18,7 +18,7 @@ const FoodPartnerRegister = () => {
     const password = e.target.password.value;
     const address = e.target.address.value;
 
-    const response = await axios.post("http://localhost:3000/api/auth/food-partner/register", {
+    const response = await axios.post("https://watch2eat-backend.onrender.com/api/auth/food-partner/register", {
       name:businessName,
       contactName,
       phone,
