6d292ecec16887836aa2447cd42190ec4f7a6208
Aman <143870254+aman05hub@users.noreply.github.com>
2026-02-28 23:08:14 +0530

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -15,7 +15,6 @@ const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                {/* <Route path="register" element={<ChooseRegister />} /> */}
                 <Route path="/user/register" element={<UserRegister />} />
                 <Route path="/user/login" element={<UserLogin />} />
                 <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
